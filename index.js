16826399b07862a1220687c253ad538d6def9ae9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-17 19:12:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function destructivelyAppendKitten(name) {
   return kittens;
 }
 
-function destructivelyPrependLastKitten(name) {
+function destructivelyPrependKitten(name) {
 kittens.unshift(name);
 return kittens;
 }
