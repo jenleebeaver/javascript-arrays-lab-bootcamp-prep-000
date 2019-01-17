033a588dc5e1a1eead7b53424b20985e4a4b7304
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-17 19:02:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] // var kittensString = "foo"
 
+function destructivelyAppendKitten(kittens) {
+  kittens.push(kittens);
+  return kittens;
+}
 // Add your functions and code here
